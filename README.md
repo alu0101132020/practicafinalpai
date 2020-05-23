82e8d0d5d27fce3d1699812de288b97ef78d4ea3
Manuel Carrera <alu0101132020@ull.edu.es>
2020-05-23 20:17:26 +0100

    Subida a github personal

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-# PAI Lab assignment (lab, class presentation with slides) structure
+## Práctica final para la asignatura de programación de aplicaciones interactivas.
 
-This directory is organised as follows:
+En la asignatura hemos realizado una gran variedad de actividades usando JavaScript,
+HTML, CSS, metodología de trabajo TDD, diagramas UML de las distintas clases implementadas...
+Y dentro de esta variedad de actividades hemos hecho desde trabajo con ficheros en
+formato json, hasta algoritmos simples como el de la malla convexa (quickhull) junto
+a su representación gráfica en HTML, hasta implementaciones del conocido en el mundo
+de la computación y programación Juego de la vida del difunto Conway.
 
-      .
-      ├── coverage   # Code coverage reports files
-      ├── docs       # All kind of project documentation, including JSDoc files
-      ├── image      # Image resources 
-      ├── tests      # Source code for tests
-      ├── src        # Source code, including JS, CSS, HTML
-
-For each assignment there should be a markdown file with the assignment description.
-Find that file and follow the instructions on it.
-
-**Before** the evaluation session of the assignment, **do not forget** to upload the (git) link to your repository to the Moodle classroom.
+Este repositorio contiene una pequeña selección de algunas actividades realizadas
+indexadas mediante varias webs para poder visualizar el trabajo realizado.
---
+++ src/main1.js
@@ -0,0 +1,95 @@
+/**
+ *  Escuela Superior de Ingeniería y Tecnología
+ *  Grado en Ingeniería Informática
+ *  Asignatura: Programación de Aplicaciones Interactivas
+ *  Curso: 3º
+ *  Práctica 13. Bouncing Ball. Programación Gráfica, Orientada a Objetos y dirigida por eventos en JS.
+ *  @author Manuel Andrés Carrera Galafate <alu0101132020@ull.edu.es>
+ *  @since 16/05/2020
+ *  @desc Programa principal.
+ *  References:
+ *              Enunciado de la práctica:
+ *              https://github.com/fsande/PAI-P13-BouncingBall/blob/master/2019-2020_p13_BouncingBall.md
+ *  Repositorio git con este (y otros) códigos:
+ *              https://github.com/ULL-ESIT-INF-PAI-2019-2020/pai-p13-bouncing-ball-alu0101132020
+ *
+ *  @file Fichero que contiene la implementación de la clase pantalla.
+ *
+ *  @version  16/05/2020 - Creación (primera versión) del código.
+ */
+
+'use strict';
+
+
+let BouncingScreenClass = null;
+
+if (typeof(window) === 'undefined') {
+  BouncingScreenClass = require('../src/bouncing-screen');
+} else {
+  BouncingScreenClass = BouncingScreen;
+}
+
+/**
+ * @description Función que inicia la bola y empieza la animación.
+ */
+const start= () => {
+  screen = new BouncingScreenClass(document.getElementById('bounce'));
+  screen.animation();
+  drawTime();
+}
+
+
+/**
+ * @description Función que modifica el color html del fondo.
+ */
+const backgroundHTML = () => {
+  const color = document.getElementById('background');
+  screen.backgroundColor = color.value;
+}
+
+/**
+ * @description Función que modifica el color html del fondo.
+ */
+const colorBall = () => {
+  const color = document.getElementById('colorball');
+  if (color.value === 'Random') {
+    screen.balls[0].color = screen.getRandomColor();
+  } else if (color.value === 'By speed') {
+
+  } else {
+    screen.balls[0].color = color.value;
+  }
+}
+
+/**
+ * @description Función que controla las modificaciones sobre la velocidad
+ * en el slider.
+ */
+const updateSlider = () => {
+  const slider = document.getElementById('time-slider');
+  variableSpeed = slider.value;
+  const value = document.getElementById('time-step-value');
+  value.textContent = variableSpeed;
+  screen.delay = baseSpeed - variableSpeed;
+}
+
+/**
+ * @description Función que escribe el tiempo que lleva la simulación
+ */
+async function drawTime () {
+  while(true) {
+    await sleep(50);
+    const time = document.getElementById('time-string');
+    time.textContent = screen.time;
+  }
+}
+
+/**
+ * @description Función que pausa la partida durante un número de milisegundos.
+ * @param {int} msToWait Número de milissegundo a esperar.
+ */
+const sleep = (msToWait) => {
+  return new Promise(resolve => setTimeout(resolve, msToWait))
+}
+
+let screen;
